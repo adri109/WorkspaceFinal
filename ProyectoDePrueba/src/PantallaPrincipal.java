a1846ef832c307b04c166dd9cee81cad9e6b2b9a
Paul Vidal <pvidalm@iesalcorisa.es>
2023-05-15 09:33:51 +0200

    Prueba

--- ProyectoDePrueba/src/PantallaPrincipal.java
+++ ProyectoDePrueba/src/PantallaPrincipal.java
@@ -37,7 +37,7 @@ public class PantallaPrincipal extends JFrame {
 	public PantallaPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		setBounds(100, 100, 923, 541);
+		setBounds(100, 100, 323, 241);
 
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
